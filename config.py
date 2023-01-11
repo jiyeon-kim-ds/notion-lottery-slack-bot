7bf2b8a81b5c17eb2d1a0cada71c232033695d0f
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2023-01-11 10:09:25 +0900

    Modify way to load environment variables

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
 
-DB_ID               = os.getenv("DB_ID")
-NOTION_TOKEN        = os.getenv("NOTION_TOKEN")
-SLACK_TOKEN         = os.getenv("SLACK_TOKEN")
-CLOVA_CLIENT_ID     = os.getenv("CLIENT_ID")
-CLOVA_CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_ID               : str = os.getenv("DB_ID")
+    NOTION_TOKEN        : str = os.getenv("NOTION_TOKEN")
+    SLACK_TOKEN         : str = os.getenv("SLACK_TOKEN")
+    CLOVA_CLIENT_ID     : str = os.getenv("CLIENT_ID")
+    CLOVA_CLIENT_SECRET : str = os.getenv("CLIENT_SECRET")
+
+
+settings = Settings()
