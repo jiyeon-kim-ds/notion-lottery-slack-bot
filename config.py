bffb719c481cf654aa3c353d27ec9736a8e31e39
Jiyeon Kim <jiyeon.kim.ds@gmail.com>
2023-01-11 10:04:39 +0900

    Add filtering and sentiment-analyzing methods

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
-DB_ID        = os.getenv("DB_ID")
-NOTION_TOKEN = os.getenv("NOTION_TOKEN")
-SLACK_TOKEN  = os.getenv("SLACK_TOKEN")
+DB_ID               = os.getenv("DB_ID")
+NOTION_TOKEN        = os.getenv("NOTION_TOKEN")
+SLACK_TOKEN         = os.getenv("SLACK_TOKEN")
+CLOVA_CLIENT_ID     = os.getenv("CLIENT_ID")
+CLOVA_CLIENT_SECRET = os.getenv("CLIENT_SECRET")
--- main.py
+++ main.py
@@ -1,22 +1,34 @@
+import urllib.parse
+
 from fastapi import FastAPI, Request
 
-from dependencies import pick_page, get_channel_id, send_slack_msg
+from dependencies import pick_page, get_element_from_message, send_slack_msg, filter_by_tag, get_notion_pages
 
 app = FastAPI()
 
 
 @app.post("/random")
-async def get_random_pick(
+async def post_random_pick(
     request: Request
 ):
     data = await request.body()
 
-    channel_id = get_channel_id(data)
+    channel_id = get_element_from_message(data, 'channel_id')
+    msg = get_element_from_message(data, 'text')
+    decoded_msg = urllib.parse.unquote(msg)
 
     if channel_id is None:
         return {"message": "다른 채널에서 이용해주세요."}
 
-    message = pick_page()
+    if decoded_msg:
+        pages = filter_by_tag(decoded_msg)
+        if not pages:
+            return {"message": "해당 태그에 관한 결과가 없습니다."}
+    else:
+        pages = get_notion_pages()
+
+    # TODO: pick page/write message 분리
+    message = pick_page(pages)
 
     res = send_slack_msg(message, channel_id)
 
